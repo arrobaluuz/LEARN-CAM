0d667f8b7995f38dd5096ad1f63b8f9258522770
jcerrito <jcerritocampos@gmail.com>
2021-10-29 15:02:47 -0500

    Actualice archivo learn-cam

--- camara/learm-cam.py
+++ camara/learm-cam.py
@@ -9,7 +9,7 @@ while(flag):
     cv2.imshow("Learn-Cam",frame)
     k = cv2.waitKey(1) & 0xFF
     if k == ord (' '): #Presione la tecla espacio para ingresar a la siguiente operación de guardado de imágenes
-        cv2.imwrite("./images/" + str(uuid.uuid4()) + ".jpg", frame)
+        cv2.imwrite("./photos/" + str(uuid.uuid4()) + ".jpg", frame)
         print(cap.get(3))
         print(cap.get(4))
     elif k == ord ('q') or k == ord('Q'): #Presione la tecla q, el programa sale
