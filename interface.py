f0ea32f01b86147b32a0b17c62cadaa8d0a847d1
Victor Martinez <91493613+vic06manu@users.noreply.github.com>
2021-12-08 10:18:16 -0600

    Update interface.py

--- interface.py
+++ interface.py
@@ -2,8 +2,8 @@ from tkinter import *
 from PIL import Image, ImageTk
 import cv2
 import sys
-import numpy as np
-import uuid
+import time
+
 
 def onClossing():
     root.quit()
@@ -23,19 +23,23 @@ def callback():
     else:
         onClossing()
 
-url = "http://192.168.0.6:4747/video"
-cap = cv2.VideoCapture(0)
+def foto():
+    ret, frame = cap.read()
+    img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    if ret:
+        cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".png", img)
 
+
+def reset():
+    print("hola")
+
+url = "http://192.168.0.6:4747/video"
+cap = cv2.VideoCapture(url)
 
 if cap.isOpened():
     print("Camara Iniciada")
 else:
     sys.exit("camara desconectada")
-
-def foto():
-    print("foto")
-def reset():
-    print("hola")
 
 root = Tk()
 root.protocol("WM_DELETE_WINDOW", onClossing)
@@ -44,13 +48,18 @@ root.title("Traductor")
 
 label = Label(root)
 label.grid(column=0, row=1)
+
 label1 = Label(root)
 label1.grid(column=1, row=1)
 
 button1 = Button(root, text="Tomar Foto", command=foto)
-button1.grid(column=0, row=2)
-button2 = Button(root, text="Reutilizar", command=reset)
+button1.grid(column=0, row=2, pady=20)
+
+button2 = Button(root, text="Reiniciar", command=reset)
 button2.grid(column=1, row=2, padx=20)
+
+button2 = Button(root, text="Salir", command=quit)
+button2.grid(column=1, row=4, padx=5, pady=20)
 
 root.after(1,callback)
 
