0caf9ec286663821e372bc2a406ad76ba30c79b2
Luz Pérez <18030636@itcelaya.edu.mx>
2021-12-08 13:26:03 -0600

    Clasificación desde la interface

--- interface.py
+++ interface.py
@@ -1,9 +1,41 @@
 from tkinter import *
-from PIL import Image, ImageTk
-import cv2
+from PIL import ImageTk
 import sys
 import time
 
+import numpy as np
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+from PIL import Image
+import cv2
+
+longitud, altura = 100 , 100
+modelo = './modelo/modelo.h5'
+pesos='./modelo/pesos.h5'
+cnn = load_model(modelo)
+cnn.load_weights(pesos)
+
+def predict(file):
+  x = load_img(file, target_size=(longitud, altura))
+  x = img_to_array(x)
+  x = np.expand_dims(x, axis=0)
+  array = cnn.predict(x)
+  result = array[0]
+  answer = np.argmax(result)
+  if answer == 0:
+    im = Image.open('./LSM/mango-lsm.png')
+    im.show()
+
+  elif answer == 1:
+    im = Image.open('./LSM/manzana-lsm.png')
+    im.show()
+  elif answer == 2:
+    im = Image.open('./LSM/pera-lsm.png')
+    im.show()
+  elif answer == 3:
+    im = Image.open('./LSM/platano-lsm.png')
+    im.show()
+  return answer
 
 def onClossing():
     root.quit()
@@ -25,15 +57,14 @@ def callback():
 
 def foto():
     ret, frame = cap.read()
-    img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     if ret:
-        cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".png", img)
-
+        cv2.imwrite("./camara/photos/frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".png", frame)
+        predict("./camara/photos/frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".png")
 
 def reset():
     print("hola")
 
-url = "http://192.168.0.6:4747/video"
+url = "http://192.168.1.74:4747/video"
 cap = cv2.VideoCapture(url)
 
 if cap.isOpened():
@@ -55,11 +86,13 @@ label1.grid(column=1, row=1)
 button1 = Button(root, text="Tomar Foto", command=foto)
 button1.grid(column=0, row=2, pady=20)
 
+
 button2 = Button(root, text="Reiniciar", command=reset)
 button2.grid(column=1, row=2, padx=20)
 
 button2 = Button(root, text="Salir", command=quit)
 button2.grid(column=1, row=4, padx=5, pady=20)
+
 
 root.after(1,callback)
 
--- train.py
+++ train.py
@@ -1,25 +1,24 @@
 import os
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras import optimizers
-from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator #preprocesar imagenes
+from tensorflow.python.keras import optimizers #entrenar algoritmo
+from tensorflow.python.keras.models import Sequential #permite hacer redes neuronales secuenciales (capas en orden)
 from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Activation
-from tensorflow.python.keras.layers import  Convolution2D, MaxPooling2D
+from tensorflow.python.keras.layers import  Convolution2D, MaxPooling2D #capas de convulsiones
 from tensorflow.python.keras import backend as K
 
 K.clear_session()
-
+#Almacenar el directorio de imagenes
 data_entrenamiento = './data/entrenamiento'
 data_validacion = './data/validacion'
 
-#Parameters
-
-epocas=20
-longitud, altura = 100, 100
-batch_size = 5
-pasos = 7 #si hay más imagenes por ser entrenadas, nay que cmabiar este valor por uno más alto
-validation_steps = 10
-filtrosConv1 = 32
-filtrosConv2 = 64
+#Parameteros de red neuronal
+epocas=20 #numero de veces a iterar
+longitud, altura = 100, 100 #tamaño a procesar imagenes (px)
+batch_size = 26 #número de imagenes en cada paso
+pasos = 10 #número de veces a procesar información
+validation_steps = 10 #correr validación
+filtrosConv1 = 32 #despues de conv1 tiene un profundidad de 32
+filtrosConv2 = 64 #despues de conv2 tiene un profundidad de 64
 tamano_filtro1 = (3, 3)
 tamano_filtro2 = (2, 2)
 tamano_pool = (2, 2)
@@ -27,13 +26,11 @@ clases = 4
 lr = 0.0005
 
 #preprocesamiento de imagenes
-
 entrenamiento_datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.3,
     zoom_range=0.3,
     horizontal_flip=True)
-
 validacion_datagen = ImageDataGenerator(rescale=1./255)
 
 imagen_entrenamiento = entrenamiento_datagen.flow_from_directory(
